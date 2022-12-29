82cb4f6a96d5c0dae22ee4f3e7d3b137f7f26ff5
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-29 19:10:53 -0300

    Implementando nosso Decorator.

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,9 +1,18 @@
 export function logarTempoDeExecucao () {
     return function(
-        target: any, 
-        propertyKey: string,
-        descriptor: PropertyDescriptor 
+        target: any, // Recebe o construtor ou o Prototype da classe. 
+        propertyKey: string, // Nome do método que será invocado no decorator.
+        descriptor: PropertyDescriptor // Objeto para definir o comportamento.
     ) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function() {
+            const t1 = performance.now();
+            const retorno = metodoOriginal()
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000} segundos.`)
+            retorno
+        }
+        
         return descriptor;
     }
 }
