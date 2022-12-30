b50fcc1b12f886aab3e263f5550d59ed70db633b
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-30 18:20:21 -0300

    Object.defineProperty.

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -3,9 +3,21 @@ export function domInjector(seletor: string) {
         target: any,
         propertyKey: string
     ) {
+        // Este comando só roda a cada injeção do decorator.
+        console.log(`Modificando do prototype ${target.constructor.name}
+            e adicionando getter para a propriedade ${propertyKey}...`)
+        // Este getter é invocado sempre que o método/propriedade identificado
+        // por propertyKey for invocado.
         const getter = function() {
             const elemento = document.querySelector(seletor);
+            console.log(`Buscando o elemento do DOM com o seletor 
+            ${seletor} para injetar em ${propertyKey}.`);
             return elemento;
         }
+        Object.defineProperty(
+            target, 
+            propertyKey, 
+            { get: getter }
+        );
     }
 }
