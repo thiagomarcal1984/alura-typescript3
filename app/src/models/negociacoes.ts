5c89cf143e00661f837be32b0c5d916e27b3062b
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-12-31 17:17:19 -0300

    Interface com Generics.

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
+import { Comparavel } from '../interfaces/comparavel.js';
 import { Imprimivel } from '../utils/imprimivel.js';
 import { Negociacao } from './negociacao.js';
 
-export class Negociacoes implements Imprimivel {
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
     private negociacoes: Negociacao[] = [];
 
     public adiciona(negociacao: Negociacao) {
@@ -14,5 +15,9 @@ export class Negociacoes implements Imprimivel {
 
     public paraTexto(): string {
         return JSON.stringify(this.negociacoes, null, 2);
+    }
+
+    public ehIgual(objeto: Negociacoes): boolean {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(objeto.lista());
     }
 }
